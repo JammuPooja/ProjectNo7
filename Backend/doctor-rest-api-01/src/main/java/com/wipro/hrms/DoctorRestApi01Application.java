e5bff8f9f9b34206f0a627f3bb74ca4bf27cc692
JammuPooja <poojajammu2828@gmail.com>
2022-06-25 19:39:30 +0530

    firstcommit

---
+++ Backend/doctor-rest-api-01/src/main/java/com/wipro/hrms/DoctorRestApi01Application.java
@@ -0,0 +1,13 @@
+package com.wipro.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorRestApi01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorRestApi01Application.class, args);
+	}
+
+}
